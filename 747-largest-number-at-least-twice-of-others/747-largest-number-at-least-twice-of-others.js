601d83dd333fc4792cb96b1d34dd71014236c5fe
YJ LEE <82587107+yujleee@users.noreply.github.com>
2022-04-20 18:03:42 +0900

    Time: 84 ms (43.49%), Space: 42.1 MB (58.81%) - LeetHub

---
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+    if (nums.length === 1) return 0;
+
+    let maxNumber = Math.max(...nums);
+
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (maxNumber === nums[i]) {
+            index = i;
+            continue;
+        }
+
+        if (maxNumber < nums[i] * 2) {
+            return -1;
+        }
+    }
+    
+    return index;
+};

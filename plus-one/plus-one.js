09315780f5f110b98bbf78dc1ba4966f3e34c69a
YJ LEE <82587107+yujleee@users.noreply.github.com>
2022-04-26 17:18:18 +0900

    Time: 110 ms, Memory: 41.8 MB - LeetHub

--- plus-one/plus-one.js
+++ plus-one/plus-one.js
@@ -2,13 +2,25 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {
-   let nums = [...digits];
-  let joinedNumber = BigInt(nums.join(''));
-  joinedNumber++;
+let plusOne = function(digits) {
+//   let nums = [...digits];
+//   let joinedNumber = BigInt(nums.join(''));
+//   joinedNumber++;
 
-  joinedNumber = joinedNumber.toString();
+//   joinedNumber = joinedNumber.toString();
 
-  return joinedNumber.split('');
+//   return joinedNumber.split('');
     
+    
+    for(let i=digits.length-1; i>-1; i--){
+        if(digits[i] != 9){
+            digits[i]++;
+            return digits;
+        } else{
+            digits[i] = 0;
+        }
+    }
+    digits.unshift(1);
+    
+    return digits;
 };

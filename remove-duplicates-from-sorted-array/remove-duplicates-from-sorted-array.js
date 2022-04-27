ed369fee97e34ee555ee599771e71af02cf9f72e
YJ LEE <82587107+yujleee@users.noreply.github.com>
2022-04-27 15:34:06 +0900

    Time: 150 ms, Memory: 44.6 MB - LeetHub

---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+      i--;
+    } else continue;
+  }
+
+  return nums.length;    
+};

0fca237373836ba4540f87d34686ddff3b028644
YJ LEE <82587107+yujleee@users.noreply.github.com>
2022-04-27 22:56:47 +0900

    Time: 82 ms (78.48%), Space: 44.7 MB (68.09%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+ if (nums.length === 0) return 0;
+
+  // 투 포인터 사용해보기.
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[count] = nums[i];
+      count++;
+    }
+  }
+
+  return count;    
+    
+   
+//  splice 써보기     
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === nums[i + 1]) {
+//       nums.splice(i, 1);
+//       i--;
+//     } else continue;
+//   }
+
+//   return nums.length;    
+};

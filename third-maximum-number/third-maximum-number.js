8ec67fdbaa818952b001ec12bb48f6a104b42001
YJ LEE <82587107+yujleee@users.noreply.github.com>
2022-05-01 21:13:00 +0900

    Time: 62 ms, Memory: 43.8 MB - LeetHub

---
+++ third-maximum-number/third-maximum-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+  // 내림차순 정렬
+  let sorting = nums.sort((a, b) => b - a);
+
+  // set으로 중복 제거
+  let set = new Set(sorting);
+
+  if (set.size < 3) return Math.max(...nums);
+
+  return [...set][2];
+};

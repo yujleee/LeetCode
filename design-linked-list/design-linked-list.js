73b055af695a04a3f703341076eb9696da66b12d
YJ LEE <82587107+yujleee@users.noreply.github.com>
2022-05-03 09:25:41 +0900

    Time: 145 ms, Memory: 50.6 MB - LeetHub

--- design-linked-list/design-linked-list.js
+++ design-linked-list/design-linked-list.js
@@ -1,111 +1,120 @@
-class ListNode {
-  constructor(val, next) {
-    this.val = val == undefined ? 0 : val;
-    this.next = next == undefined ? null : next;
-  }
+function Node(val) {
+  this.val = val;
+  this.next = null;
 }
 
 /**
  * Initialize your data structure here.
  */
-var MyLinkedList = function() {
-    this.head = null;
+var MyLinkedList = function () {
+  this.head = null;
+  this.length = 0;
+};
+
+MyLinkedList.prototype.getLength = function () {
+  let length = 0;
+  let curr = this.head;
+
+  while (curr !== null) {
+    curr = curr.next;
+    length++;
+  }
+
+  return length;
 };
 
 /**
- * Get the value of the index-th node in the linked list. If the index is invalid, return -1. 
+ * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
  * @param {number} index
  * @return {number}
  */
-MyLinkedList.prototype.get = function(index) {
-    if (index < 0 || index >= this.getLength()) return -1;
-    let cur = this.head;
-    for (let i = 0; i < index; i++) {
-        cur = cur.next;
-    }
-    return cur.val;
+MyLinkedList.prototype.get = function (index) {
+  if (index < 0 || index >= this.getLength()) return -1;
+  let curr = this.head;
+  for (let i = 0; i < index; i++) {
+    curr = curr.next;
+  }
+  return curr.val;
 };
 
 /**
- * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. 
+ * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtHead = function(val) {
-    let node = new ListNode(val);
-    node.next = this.head;
-    this.head = node;
+MyLinkedList.prototype.addAtHead = function (val) {
+  let node = new Node(val);
+  node.next = this.head;
+  this.head = node;
+
+  this.length++;
 };
 
 /**
- * Append a node of value val to the last element of the linked list. 
+ * Append a node of value val to the last element of the linked list.
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtTail = function(val) {
-    if (this.head == null) {
-        this.addAtHead(val);
-        return;
-    }
-    let node = new ListNode(val);
-    let cur = this.head;
-    while (cur.next !== null) {
-        cur = cur.next;
-    }
-    cur.next = node;
+MyLinkedList.prototype.addAtTail = function (val) {
+  if (this.head == null) {
+    this.addAtHead(val);
+    return;
+  }
+  let node = new Node(val);
+  let curr = this.head;
+  while (curr.next !== null) {
+    curr = curr.next;
+  }
+  curr.next = node; // 끝지점에서 새로운 node
+  this.length++;
 };
 
 /**
- * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. 
- * @param {number} index 
+ * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+ * @param {number} index
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtIndex = function(index, val) {
-    if (index === 0) {
-        this.addAtHead(val);
-        return;
-    }
-    if (index === this.getLength()) {
-        this.addAtTail(val);
-        return;
-    }
-    if (index > this.getLength()) return;
-        
-    let cur = this.head;
-    let node = new ListNode(val);
-    for (let i = 0; i < index-1; i++) {
-        cur = cur.next;
-    }
-    let next = cur.next;
-    cur.next = node;
-    node.next = next;
+MyLinkedList.prototype.addAtIndex = function (index, val) {
+  if (index === 0) {
+    this.addAtHead(val);
+    return;
+  }
+  if (index === this.getLength()) {
+    this.addAtTail(val);
+    return;
+  }
+  if (index > this.getLength()) return;
+
+  let curr = this.head;
+  let node = new Node(val);
+  for (let i = 0; i < index - 1; i++) {
+    curr = curr.next;
+  }
+  let next = curr.next;
+  curr.next = node;
+  node.next = next;
+  this.length++;
 };
 
 /**
- * Delete the index-th node in the linked list, if the index is valid. 
+ * Delete the index-th node in the linked list, if the index is valid.
  * @param {number} index
  * @return {void}
  */
-MyLinkedList.prototype.deleteAtIndex = function(index) {
-    if (index < 0 || index >= this.getLength()) return;
-    if (index == 0) {
-        this.head = this.head.next;
-        return;
-    }
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+  if (index < 0 || index >= this.getLength()) return;
+  if (index === 0) {
+    this.head = this.head.next;
+    return;
+  }
 
-    let cur = this.head;
-    for (let i = 0; i < index-1; i++) {
-        cur = cur.next;
-    }
-    cur.next = cur.next.next;
-};
+  let curr = this.head;
 
-MyLinkedList.prototype.getLength = function() {
-    let len = 0, cur = this.head;
-    while (cur !== null) {
-        cur = cur.next;
-        len++;
-    }
-    return len;
+  for (let i = 0; i < index - 1; i++) {
+    curr = curr.next;
+  }
+  curr.next = curr.next.next;
+
+  this.length--;
 };
